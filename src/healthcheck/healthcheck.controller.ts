2eaa479495d6f19137405fc9ec4d0df948da018e
Illia Kornyk <krekerbrow@gmail.com>
2024-09-22 14:26:16 +0300

    feat: add swagger docs

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,8 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('healthcheck')
 @Controller('healthcheck')
 export class HealthcheckController {
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Service is healthy',
+    schema: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          enum: ['UP'],
+        },
+        date: { type: 'string' },
+      },
+    },
+  })
   healthCheck() {
     return {
       status: 'UP',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Messenger App')
+    .setDescription('A simple messenger application API')
+    .setVersion('1.0')
+    .addTag('messenger')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

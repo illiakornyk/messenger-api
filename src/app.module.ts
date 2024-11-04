ef6ef594b476a56aa44cc1f69b57385ccca4d181
Illia Kornyk <krekerbrow@gmail.com>
2024-11-04 09:26:57 +0200

    feat: connect to pg db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST') || 'localhost',
+        port: configService.get<number>('POSTGRES_PORT') || 5432,
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
     HealthcheckModule,
-    DatabaseModule,
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })

142cbfd6f3d0ef73731e2ce1e5afa88202eaf278
Illia Kornyk <krekerbrow@gmail.com>
2024-11-04 08:48:03 +0200

    feat: add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthcheckModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    HealthcheckModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
